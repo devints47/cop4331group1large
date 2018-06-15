3a601ef28672588f2aa716a8aa3ff809d3711c78
Camilo Lozano <camilozano98@gmail.com>
2018-06-14 21:37:13 -0400

    Initial Login Page for App, not functional yet

---
+++ mobile/dnd/App.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react';
+import {Text,View,AppRegistry} from 'react-native';
+import Login from './app/components/Login/Login';
+
+
+export default class App extends Component {
+  render() {
+    return (
+        <Login />
+    );
+  }
+}
---
+++ mobile/dnd/app/components/Login/Login.js
@@ -0,0 +1,62 @@
+
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  AppRegistry,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Image
+} from 'react-native';
+import LoginForm from './LoginForm';
+import * as Animate from 'react-native-animatable';
+
+
+export default class Login extends Component {
+  render() {
+    return (
+      <KeyboardAvoidingView behavior='padding' style={styles.main}>
+          	<Animate.View style={styles.container} animation='zoomIn' iterationCount={1}>
+				<Image
+				source={require('../../assets/images/logo.png')}
+				style={styles.logo}
+				/>
+				<Text style={styles.text}>
+					Dungeons and Dragons
+				</Text>
+
+          	</Animate.View>
+			
+			<View style={styles.formContainer}>
+				<LoginForm />
+			</View>
+
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    main:{
+        backgroundColor:'#d2c4b4',
+        flex:1,
+    },
+    container:{
+      alignItems:'center',
+      flexGrow:1,
+      justifyContent:'center',
+	},
+	logo:{
+		height:200,
+		width:200
+	},
+	text:{
+		color:'rgba(34,24,13,0.4)',
+		justifyContent: 'center',
+		textAlign: 'center'
+	}
+});
+
+
+AppRegistry.registerComponent('Login', ()=>Login)
+
