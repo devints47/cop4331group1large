167311bd4eb2dafb76db854451566e8de5bed2b1
umadapuma <umadapuma@hotmail.com>
2018-06-17 19:16:46 -0400

    character page(router.js)
    need to add modal

--- mobile/dnd/app/components/Login/router.js
+++ mobile/dnd/app/components/Login/router.js
@@ -1,12 +1,68 @@
 import React, { Component } from 'react';
-import {Text,View,AppRegistry,StyleSheet, TextInput} from 'react-native';
-
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+import {Modal, Text, TouchableHighlight} from 'react-native';
 export default class router extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+    
+    
+  }
+
   render() {
     return (
-      <Text>Hello world!</Text>
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Add Character"
+            color="#4CAF50"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="My Characters"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Delete Character"
+            color="#f44336"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="This looks great!"
+          />
+          <Button
+            onPress={this._onPressButton}
+            title="OK!"
+            color="#841584"
+          />
+        </View>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
+
+
 
 AppRegistry.registerComponent('router', ()=>router)
