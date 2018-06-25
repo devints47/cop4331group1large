7522928749a2a5d4d116f19f998b8d845209dbef
DeepakGunturu <33680754+DeepakGunturu@users.noreply.github.com>
2018-06-24 20:18:28 -0400

    Updated drawer menu and navigation

--- mobile/dnd/App.js
+++ mobile/dnd/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {Text,View,AppRegistry} from 'react-native';
 import Login from './app/components/Login/Login';
-import MainAppPage from './app/components/Home/MainAppPage';
 import router from './app/components/Home/router';
 import { createStackNavigator,DrawerNavigator,StackNavigator} from 'react-navigation'; 
 
@@ -9,29 +8,12 @@ import { createStackNavigator,DrawerNavigator,StackNavigator} from 'react-naviga
 const AppStackNavigator = createStackNavigator(
   {
   Home: Login,
-  Router: MainAppPage,
+  Router: router,
   }
 );
 
-const MainScreenNavigator = StackNavigator({
-  MainAppPage: { screen: MainAppPage },
-  Home: {screen: Login},
-  Router: {screen : MainAppPage}
 
-});
-
-const Drawer = DrawerNavigator(
-  {
-    Main: { screen: MainScreenNavigator }
-  },
-  {
-    contentComponent: router,
-    drawerWidth: 200
-  }
-);
-
-class App extends React.Component{
-  
+export default class App extends React.Component{
   
   render() {
 
@@ -41,4 +23,4 @@ class App extends React.Component{
   }
 }
 
-export default App;
+
